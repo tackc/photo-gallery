53bd1bdbe62a4b615e51c015c6453bf9fabfd4a0
Chris Tack <chris_tack@mac.com>
2019-10-31 08:29:14 -0700

    Initial project creation using create-react-app

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './header.css';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header className="Header">
+                <h1>Photo Gallery</h1>
+            </header>
+        )
+    }
+}
+
+export default Header;
