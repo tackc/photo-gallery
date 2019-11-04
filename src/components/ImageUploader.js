16a7e4896ac94b1538bc2a2a53fe87192e128445
Chris Tack <chris_tack@mac.com>
2019-11-04 07:42:15 -0800

    Add css to image upload component

--- src/components/ImageUploader.js
+++ src/components/ImageUploader.js
@@ -1,18 +1,11 @@
 import React, { Component } from 'react';
+import './imageUploader.css';
 
 class ImageUploader extends Component {
     render() {
         return(
             <div className="container">
                 <div className="row">
-                    <div className="col-md-6">
-                    <form method="post" action="#" id="#">
-                        <div className="form-group files">
-                        <label>Upload Your File </label>
-                        <input type="file" className="form-control" multiple />
-                        </div>
-                    </form>
-                    </div>
                     <div className="col-md-6">
                     <form method="post" action="#" id="#">
                         <div className="form-group files color">
