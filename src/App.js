f212b05af4535be628d90565fa86336240ec84ad
Chris Tack <chris_tack@mac.com>
2019-11-07 19:30:40 -0800

    work on calculateAspectRatio function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import './App.css';
 import Header from './components/Header';
 import AllPhotos from './components/AllPhotos';
 import ImageUploader from './components/ImageUploader';
+
+import './App.css';
 
 class App extends Component {
   constructor(props) {
@@ -21,12 +22,29 @@ class App extends Component {
         },
       ]
     }
-    this.addMoreImages = this.addMoreImages.bind(this)
+    this.addMoreImages = this.addMoreImages.bind(this);
+    this.calculateAspectRatio = this.calculateAspectRatio.bind(this)
   }
 
   addMoreImages() {
+    // assign width and height approximate aspect ratio to incoming image
+
     // when images are added via the upload button, add them to "photos" in state
     this.photos.push({src: "", width: null, height: null},)
+  }
+
+  calculateAspectRatio(image) {
+    let width = image.naturalWidth
+    let height = image.naturalHeight
+    // divide image width by image height to determine aspect ratio
+    if (width > height) {
+      return [width, height] = [4,3]
+    } else if (height > width) {
+      return [width, height] = [3,4]
+    } else if (width === height) {
+      return [width, height] = [1,1]
+    }
+    // assign width and height that based on results
   }
 
   render() {
